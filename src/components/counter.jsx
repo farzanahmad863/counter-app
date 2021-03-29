a9130a1a3f21a2395f69c02e0dab10e252dfef8e
farzanahmad863 <farzan.ahmad863@yahoo.com>
2021-03-29 23:26:29 +0200

    updated counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state= {
-        count:0
+        count:1,
+        tags: []
     };
     render() { 
         return(
         <React.Fragment>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary">Increment</button>
+            {this.renderTags()}
         </React.Fragment>
         );
+    }
+    renderTags(){
+        if(this.state.tags.length===0) return <p>There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>;
     }
     formatCount(){
         const {count}=this.state;
         return count===0 ? "Zero" : count;
+    }
+    getBadgeClasses(){
+        let classes= "badge m-2 badge-";
+        classes+= this.state.count===0 ? "warning" : "primary";
+        return classes
     }
 }
  
